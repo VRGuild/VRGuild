ba40670a636f34d5970d0f1fd64112cb15574151
liebespaar93 <liebespaar93@gmail.com>
2024-10-10 13:53:08 +0900

    Update Resgister UI
    Resgister UI updated

---
+++ VRGuild/Source/VRGuild/Public/Register/CWGProficiency.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CWGProficiency.generated.h"
+
+USTRUCT(BlueprintType, Atomic)
+struct FProficiency
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	FProficiency() : Name("Empty"), Index(0) {};
+
+	FProficiency(FString name, int32 index) : Name(name), Index(index) { };
+
+	UPROPERTY(BlueprintReadWrite)
+	FString Name;
+
+	UPROPERTY(BlueprintReadWrite)
+	int32 Index;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class VRGUILD_API UCWGProficiency : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
