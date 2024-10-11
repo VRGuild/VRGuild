6a64a7e9f639aca69f8655de89206e8b1c641257
liebespaar93 <liebespaar93@gmail.com>
2024-10-11 11:24:21 +0900

    ⚡️ Feat : Ether api
    - [x] ehter post 완료

---
+++ VRGuild/Source/VRGuild/Public/Global/API/CACEtherAPI.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Global/API/CACBaseAPI.h"
+#include "CACEtherAPI.generated.h"
+
+USTRUCT(BlueprintType, Atomic)
+struct FJsonEther
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+	FJsonEther() : Amount(0), Reason("") {};
+
+	FJsonEther(int32 amount ,FString reason) : Amount(amount), Reason(reason) { };
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 Amount;
+
+	UPROPERTY(BlueprintReadOnly)
+	FString Reason;
+};
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class VRGUILD_API UCACEtherAPI : public UCACBaseAPI
+{
+	GENERATED_BODY()
+public:
+	UCACEtherAPI();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	virtual void InitializeComponent() override;
+
+	class ACPCBasePlayerController* OwnerPlayerController;
+
+	UFUNCTION(BlueprintCallable)
+	void EtherPostCall();
+
+	virtual void OnSuccessAPI() override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+
+	void OnEtherComple();
+
+};
