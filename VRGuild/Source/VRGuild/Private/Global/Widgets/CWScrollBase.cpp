6e92e6609737bf8a945ef4a3c598e0cfacf1edf6
1Codelearner <exon0125@gmail.com>
2024-10-18 10:57:07 +0900

    Working on UI poster interaction.

---
+++ VRGuild/Source/VRGuild/Private/Global/Widgets/CWScrollBase.cpp
@@ -0,0 +1,92 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Global/Widgets/CWScrollBase.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Components/ScrollBox.h"
+
+void UCWScrollBase::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (WidgetToDisplay)
+	{
+		if (auto Widget = CreateWidget(WidgetToDisplay)) 
+		{
+			ScrollBox->AddChild(Widget);
+			ScrollBox->SetConsumeMouseWheel(EConsumeMouseWheel::Never);
+		}
+	}
+
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetOwningPlayer()->GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(ScrollingInputContext, 0);
+	}
+
+	UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(GetOwningPlayer()->InputComponent);
+	if (EnhancedInput)
+	{
+		EnhancedInput->BindAction(IA_Scroll, ETriggerEvent::Triggered, this, &UCWScrollBase::OnScrollActive);
+	}
+}
+
+void UCWScrollBase::NativeDestruct()
+{
+	if (GetOwningPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetOwningPlayer()->GetLocalPlayer()))
+		{
+			Subsystem->RemoveMappingContext(ScrollingInputContext);
+		}
+	}
+	
+	Super::NativeDestruct();
+}
+
+void UCWScrollBase::OnScrollActive(const FInputActionValue& Action)
+{
+	float value = Action.Get<float>();
+	
+	if (IsPlayingAnimation()) return;
+
+	Offset += value * OffsetSpeed;
+
+	if (bIsHidden && Offset > 0.f)
+	{
+		Offset = 0.f;
+	}
+	else if (Offset > ScrollBox->GetScrollOffsetOfEnd())
+	{
+		Offset = ScrollBox->GetScrollOffsetOfEnd();
+	}
+	else ScrollBox->SetScrollOffset(Offset);
+
+	if (!bIsHidden)
+	{
+		if (Offset < 0.f)
+		{
+			PlayAnimForward();
+			Offset = 0.f;
+			ScrollBox->SetWheelScrollMultiplier(0.f);
+		}
+	}
+	else if (Offset > 0.f)
+	{
+		PlayAnimReverse();
+		Offset = 0.f;
+		ScrollBox->SetScrollOffset(Offset);
+	}
+}
+
+void UCWScrollBase::OnAnimationFinishedPlaying(UUMGSequencePlayer& Player)
+{
+	Super::OnAnimationFinishedPlaying(Player);
+
+	if (bIsHidden)
+	{
+		bIsHidden = false;
+		ScrollBox->SetWheelScrollMultiplier(WheelScrollMultiplier);
+	}
+	else bIsHidden = true;
+}
---
+++ VRGuild/Source/VRGuild/Public/Global/Widgets/CWScrollBase.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CWScrollBase.generated.h"
+
+/**
+ * 
+ */
+
+class UScrollBox;
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class VRGUILD_API UCWScrollBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UScrollBox> ScrollBox;
+	UPROPERTY(BlueprintReadOnly, meta=(ExposeOnSpawn))
+	TSubclassOf<UUserWidget> WidgetToDisplay;
+	bool bIsHidden;
+	float Offset;
+	UPROPERTY(EditDefaultsOnly, Category = Settings)
+	float OffsetSpeed;
+	UPROPERTY(EditDefaultsOnly, Category = Settings)
+	float WheelScrollMultiplier;
+
+	UPROPERTY(EditDefaultsOnly, Category=Settings)
+	TObjectPtr<UInputMappingContext> ScrollingInputContext;
+	UPROPERTY(EditDefaultsOnly, Category = Settings)
+	TObjectPtr<UInputAction> IA_Scroll;
+
+	UFUNCTION()
+	void OnScrollActive(const FInputActionValue& Action);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void PlayAnimForward();
+	UFUNCTION(BlueprintImplementableEvent)
+	void PlayAnimReverse();
+
+	virtual void OnAnimationFinishedPlaying(UUMGSequencePlayer& Player) override;
+};
