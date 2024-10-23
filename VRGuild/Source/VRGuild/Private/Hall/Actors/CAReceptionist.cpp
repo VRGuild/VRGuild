404cf1182d13219ea8c22c18320d3ef2b8f68638
1Codelearner <exon0125@gmail.com>
2024-10-23 23:01:47 +0900

    Saving work on begin & end interaction with interactable NPC actor.

--- VRGuild/Source/VRGuild/Private/Hall/Actors/CAReceptionist.cpp
+++ VRGuild/Source/VRGuild/Private/Hall/Actors/CAReceptionist.cpp
@@ -6,13 +6,21 @@
 #include "GameFramework/Character.h"
 #include "Global/Components/CACInteraction.h"
 
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+void ACAReceptionist::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
 void ACAReceptionist::BeginTrace(ACharacter* Initiator)
-{	
-	if(!Initiator) return;
+{
+	if (!Initiator) return;
 
 	UE_LOG(LogTemp, Warning, TEXT("ACAReceptionist BeginTrace"));
 
-	if(auto CarryComponent = Initiator->GetComponentByClass<UCACCarry>())
+	if (auto CarryComponent = Initiator->GetComponentByClass<UCACCarry>())
 	{
 		SetTraceMessage(CarryComponent->GetMessageForNPC());
 	}
@@ -30,12 +38,102 @@ void ACAReceptionist::BeginInteract(ACharacter* Initiator)
 {
 	Super::BeginInteract(Initiator);
 
+	if (Initiator)
+	{
+		if (auto interactComp = Initiator->GetComponentByClass<UCACInteraction>())
+		{
+			interactComp->Disable();
+		}
+
+		auto EnhancedInputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(Initiator->GetController<APlayerController>()->GetLocalPlayer());
+		if (EnhancedInputSubsystem)
+		{
+			if (ensure(DefaultInputContext) && EnhancedInputSubsystem->HasMappingContext(DefaultInputContext))
+			{
+				EnhancedInputSubsystem->RemoveMappingContext(DefaultInputContext);
+			}
+
+			if (auto carryComp = Initiator->GetComponentByClass<UCACCarry>())
+			{
+				switch (carryComp->GetCarryType())
+				{
+				case ECarriedType::COMMISSION:
+				{
+					DisplayCommossionPopup(true);
+					if (ensure(ScrollInputContext) && EnhancedInputSubsystem->HasMappingContext(ScrollInputContext))
+					{
+						EnhancedInputSubsystem->RemoveMappingContext(ScrollInputContext);
+					}
+					break;
+				}
+				case ECarriedType::REGISTRATION:
+				{
+					break;
+				}
+				case ECarriedType::NONE:
+				{
+					DisplayDefaultPopup(true);
+					break;
+				}
+				}
+			}
+		}
+
+		Initiator->GetController<APlayerController>()->SetShowMouseCursor(true);
+	}
+
+
+
 	UE_LOG(LogTemp, Warning, TEXT("ACAReceptionist BeginINteract"));
 }
 
 void ACAReceptionist::EndInteract(ACharacter* Initiator)
 {
 	Super::EndInteract(Initiator);
+
+	if (Initiator)
+	{
+		if (auto InteractComp = Initiator->GetComponentByClass<UCACInteraction>())
+		{
+			InteractComp->Enable();
+		}
+
+		auto EnhancedInputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(Initiator->GetController<APlayerController>()->GetLocalPlayer());
+		if (EnhancedInputSubsystem)
+		{
+			if (ensure(DefaultInputContext) && !EnhancedInputSubsystem->HasMappingContext(DefaultInputContext))
+			{
+				EnhancedInputSubsystem->AddMappingContext(DefaultInputContext, 0);
+			}
+
+			if (auto carryComp = Initiator->GetComponentByClass<UCACCarry>())
+			{
+				switch (carryComp->GetCarryType())
+				{
+				case ECarriedType::COMMISSION:
+				{
+					DisplayCommossionPopup(true);
+					if (ensure(ScrollInputContext) && !EnhancedInputSubsystem->HasMappingContext(ScrollInputContext))
+					{
+						EnhancedInputSubsystem->AddMappingContext(ScrollInputContext, 0);
+					}
+					break;
+				}
+				case ECarriedType::REGISTRATION:
+				{
+					break;
+				}
+				case ECarriedType::NONE:
+				{
+					DisplayDefaultPopup(true);
+					break;
+				}
+				}
+			}
+		}
+
+		Initiator->GetController<APlayerController>()->SetShowMouseCursor(false);
+	}
 
 	UE_LOG(LogTemp, Warning, TEXT("ACAReceptionist EndINteract"));
 }
