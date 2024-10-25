ff179f559be212a598b9f3550bb71da50ceb24ce
liebespaar93 <liebespaar93@gmail.com>
2024-10-25 14:20:55 +0900

    update team api load

---
+++ VRGuild/Source/VRGuild/Public/Global/API/BPL/CBPLTeam.h
@@ -0,0 +1,97 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CBPLTeam.generated.h"
+
+USTRUCT(BlueprintType, Atomic)
+struct FTeamInfoAPI
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	FTeamInfoAPI() {};
+
+	UPROPERTY(BlueprintReadWrite)
+	FString teamName;
+
+	UPROPERTY(BlueprintReadWrite)
+	FString introduction;
+
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FString> memberList;
+};
+
+USTRUCT(BlueprintType, Atomic)
+struct FTeamDetailInfoAPI
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	FTeamDetailInfoAPI() {};
+
+	UPROPERTY(BlueprintReadWrite)
+	int32 teamId;
+
+	UPROPERTY(BlueprintReadWrite)
+	FString pmId;
+
+	UPROPERTY(BlueprintReadWrite)
+	FString teamName;
+
+	UPROPERTY(BlueprintReadWrite)
+	FString introduction;
+
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FString> memberList;
+
+	UPROPERTY(BlueprintReadWrite)
+	FDateTime	createdAt;
+
+	UPROPERTY(BlueprintReadWrite)
+	FDateTime	modifiedAt;
+};
+
+USTRUCT(BlueprintType, Atomic)
+struct FTeamDetailInfoDataAPI
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	FTeamDetailInfoDataAPI() {};
+
+	UPROPERTY(BlueprintReadWrite)
+	FTeamDetailInfoAPI data;
+};
+
+USTRUCT(BlueprintType, Atomic)
+struct FTeamInfoDataArrayAPI
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	FTeamInfoDataArrayAPI() {};
+
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FTeamDetailInfoAPI> data;
+};
+USTRUCT(BlueprintType, Atomic)
+struct FTeamIdAPI
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	FTeamIdAPI() {};
+
+	UPROPERTY(BlueprintReadWrite)
+	int32 teamId;
+
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FString> newMemberList;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class VRGUILD_API UCBPLTeam : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
---
+++ VRGuild/Source/VRGuild/Public/Global/API/CACTeamAPI.h
@@ -0,0 +1,93 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Global/API/BPL/CBPLTeam.h"
+
+#include "CoreMinimal.h"
+#include "Global/API/CACBaseAPI.h"
+#include "CACTeamAPI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRGUILD_API UCACTeamAPI : public UCACBaseAPI
+{
+	GENERATED_BODY()
+
+	UCACTeamAPI();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void InitializeComponent() override;
+
+	virtual void OnSuccessAPI(FHttpRequestPtr req, FHttpResponsePtr res) override;
+	virtual void OnFailAPI(FHttpRequestPtr req, FHttpResponsePtr res) override;
+
+	UFUNCTION(BlueprintCallable)
+	void TeamPostCall(FTeamInfoAPI teamInfo);
+	void TeamPostCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTeamPostCallBack(FTeamDetailInfoAPI ParseData);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnFailTeamPostCallBack();
+
+	UFUNCTION(BlueprintCallable)
+	void TeamPatchCall(FTeamInfoAPI ss);
+	void TeamPatchCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTeamPatchCallBack(FTeamDetailInfoAPI ParseData);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnFailTeamPatchCallBack();
+
+	UFUNCTION(BlueprintCallable)
+	void TeamGetCall(int32 TeamId);
+	void TeamGetCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTeamGetCallBack(FTeamDetailInfoAPI ParseData);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnFailTeamGetCallBack();
+
+	UFUNCTION(BlueprintCallable)
+	void TeamDeleteCall(int32 TeamId);
+	void TeamDeleteCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTeamDeleteCallBack();
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnFailTeamDeleteCallBack();
+
+	UFUNCTION(BlueprintCallable)
+	void TeamManagerGetCall();
+	void TeamManagerGetCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTeamManagerGetCallBack(FTeamInfoDataArrayAPI ParseData);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnFailTeamManagerGetCallBack();
+
+	UFUNCTION(BlueprintCallable)
+	void TeamMemberPostCall(FTeamIdAPI TeamId);
+	void TeamMemberPostCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTeamMemberPostCallBack(FTeamDetailInfoAPI ParseData);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnFailTeamMemberPostCallBack();
+
+	UFUNCTION(BlueprintCallable)
+	void TeamMemberDeleteCall(FTeamIdAPI TeamId);
+	void TeamMemberDeleteCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTeamMemberDeleteCallBack(FTeamDetailInfoAPI ParseData);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnFailTeamMemberDeleteCallBack();
+
+	UFUNCTION(BlueprintCallable)
+	void TeamMemberSearchGetCall();
+	void TeamMemberSearchGetCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnTeamMemberSearchGetCallBack(FTeamInfoDataArrayAPI ParseData);
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnFailTeamMemberSearchGetCallBack();
+
+};
