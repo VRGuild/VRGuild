9338752a08c75a5f989b9b433b8da90d0eb9037c
1Codelearner <exon0125@gmail.com>
2024-10-27 23:48:18 +0900

    Portal Actor

--- VRGuild/Source/VRGuild/Public/Hall/Actors/CAPortal.h
+++ VRGuild/Source/VRGuild/Public/Hall/Actors/CAPortal.h
@@ -15,8 +15,25 @@ class VRGUILD_API ACAPortal : public ACAInteractable
 	GENERATED_BODY()
 
 public:
+	ACAPortal();
+
+	virtual bool CanTrace(ACharacter* Initiator) const override;
+	virtual bool CanInteract(ACharacter* Initiator) const override;
+
 	virtual void BeginTrace(ACharacter* Initiator) override;
 	virtual void EndTrace(ACharacter* Initiator) override;
 	virtual void BeginInteract(ACharacter* Initiator) override;
 	virtual void EndInteract(ACharacter* Initiator) override;
+
+protected:
+	virtual void OnPlayerOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+	
+	virtual void OnPlayerOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex) override;
+	
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartDisplay();
+private:
+	bool bOverlapped;
 };
