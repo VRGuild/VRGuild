d875a95b908d6f6cc12674a6fef3bcb7215276b1
[Spacialist] <a01050254473@gmail.com>
2024-10-28 13:33:15 +0900

    character montage and UI fix

---
+++ VRGuild/Source/VRGuild/Public/Global/Components/CACCharacterAnimMontage.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CACCharacterAnimMontage.generated.h"
+
+
+UENUM(BlueprintType)
+enum class EAnimMontageType : uint8
+{
+	POINT UMETA(DisplayName="Point"),
+	PICKUP UMETA(DisplayName = "Pickup")
+};
+
+
+UCLASS(Blueprintable, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class VRGUILD_API UCACCharacterAnimMontage : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UCACCharacterAnimMontage();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
+	class UAnimMontage* PointAnim;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartAnimMontage(EAnimMontageType value);
+
+
+
+};
