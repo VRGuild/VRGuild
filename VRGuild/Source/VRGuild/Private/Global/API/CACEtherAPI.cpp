47573425593d1fa623e1a2eab79567902a594492
liebespaar93 <liebespaar93@gmail.com>
2024-11-05 17:10:05 +0900

    ether api created

--- VRGuild/Source/VRGuild/Private/Global/API/CACEtherAPI.cpp
+++ VRGuild/Source/VRGuild/Private/Global/API/CACEtherAPI.cpp
@@ -1,44 +1,171 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Global/API/CACEtherAPI.h"
-#include "Global/Server/CPCBasePlayerController.h"
+﻿#include "Global/API/CACEtherAPI.h"
+#include "Global/API/BPL/CBPLEther.h"
 
 UCACEtherAPI::UCACEtherAPI()
 {
-	PrimaryComponentTick.bCanEverTick = false;
-	bWantsInitializeComponent = true;
+    PrimaryComponentTick.bCanEverTick = false;
+    bWantsInitializeComponent = true;
 }
 
 void UCACEtherAPI::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 }
 
 void UCACEtherAPI::InitializeComponent()
 {
-	Super::InitializeComponent();
-
-	OwnerPlayerController = nullptr;
-	if (Owner->IsA<ACPCBasePlayerController>())
-	{
-		OwnerPlayerController = Cast<ACPCBasePlayerController>(this->Owner);
-	}
-}
-
-void UCACEtherAPI::EtherPostCall()
-{
-	FJsonEther ether = FJsonEther();
-
-	HttpPostCall<FJsonEther>(ether);
-
+    Super::InitializeComponent();
 }
 
 void UCACEtherAPI::OnSuccessAPI(FHttpRequestPtr req, FHttpResponsePtr res)
 {
-	OnEtherComple();
+    UE_LOG(LogTemp, Display, TEXT("OnSuccessAPI : %s"), *req->GetURL());
+
+    FRegexPattern CreateEtherPattern(TEXT(R"(POST\s+/api/ether$)"));
+    FRegexPattern GetEtherPattern(TEXT(R"(GET\s+/api/ether/(\d+)$)"));
+    FRegexPattern GetEtherListPattern(TEXT(R"(GET\s+/api/ether/list$)"));
+    FRegexPattern GetEtherHistoryPattern(TEXT(R"(GET\s+/api/ether/history/(\d+)$)"));
+
+    FString UrlToMatch = req->GetVerb() + TEXT(" /") + GetAPIPath(req->GetURL());
+    UE_LOG(LogTemp, Display, TEXT("URL to match: %s"), *UrlToMatch);
+
+    if (FRegexMatcher(CreateEtherPattern, UrlToMatch).FindNext())
+    {
+        EtherCreateCallBack(req, res);
+    }
+    else if (FRegexMatcher(GetEtherPattern, UrlToMatch).FindNext())
+    {
+        EtherGetCallBack(req, res);
+    }
+    else if (FRegexMatcher(GetEtherListPattern, UrlToMatch).FindNext())
+    {
+        EtherListGetCallBack(req, res);
+    }
+    else if (FRegexMatcher(GetEtherHistoryPattern, UrlToMatch).FindNext())
+    {
+        EtherHistoryGetCallBack(req, res);
+    }
 }
 
 void UCACEtherAPI::OnFailAPI(FHttpRequestPtr req, FHttpResponsePtr res)
 {
+    FString UrlToMatch = req->GetVerb() + TEXT(" /") + GetAPIPath(req->GetURL());
+    FString ErrorMessage = TEXT("알 수 없는 오류가 발생했습니다.");
+
+    // Parse error message from response
+    if (res->GetResponseCode() == 400 || res->GetResponseCode() == 401 || res->GetResponseCode() == 404)
+    {
+        TSharedPtr<FJsonObject> JsonObject;
+        TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(res->GetContentAsString());
+        if (FJsonSerializer::Deserialize(Reader, JsonObject))
+        {
+            ErrorMessage = JsonObject->GetStringField(TEXT("message"));
+        }
+    }
+
+    FRegexPattern CreateEtherPattern(TEXT(R"(POST\s+/api/ether$)"));
+    FRegexPattern GetEtherPattern(TEXT(R"(GET\s+/api/ether/(\d+)$)"));
+    FRegexPattern GetEtherListPattern(TEXT(R"(GET\s+/api/ether/list$)"));
+    FRegexPattern GetEtherHistoryPattern(TEXT(R"(GET\s+/api/ether/history/(\d+)$)"));
+
+    if (FRegexMatcher(CreateEtherPattern, UrlToMatch).FindNext())
+    {
+        OnFailEtherCreateCallBack(ErrorMessage);
+    }
+    else if (FRegexMatcher(GetEtherPattern, UrlToMatch).FindNext())
+    {
+        OnFailEtherGetCallBack(ErrorMessage);
+    }
+    else if (FRegexMatcher(GetEtherListPattern, UrlToMatch).FindNext())
+    {
+        OnFailEtherListGetCallBack();
+    }
+    else if (FRegexMatcher(GetEtherHistoryPattern, UrlToMatch).FindNext())
+    {
+        OnFailEtherHistoryGetCallBack();
+    }
+}
+
+void UCACEtherAPI::EtherCreateCall(const FEther& Ether)
+{
+    this->API = TEXT("api/ether");
+    HttpPostCall<FEther>(Ether);
+}
+
+void UCACEtherAPI::EtherCreateCallBack(FHttpRequestPtr req, FHttpResponsePtr res)
+{
+    FString JsonString = res->GetContentAsString();
+    FEtherResponse ParsedResponse = JsonPerse<FEtherResponse>(JsonString);
+
+    if (ParsedResponse.status == TEXT("success"))
+    {
+        OnEtherCreateCallBack(ParsedResponse.data);
+    }
+    else
+    {
+        OnFailEtherCreateCallBack(TEXT("에테르 생성에 실패했습니다."));
+    }
+}
+
+void UCACEtherAPI::EtherGetCall(const FString& EtherId)
+{
+    this->API = FString::Printf(TEXT("api/ether/%s"), *EtherId);
+    HttpGetCall();
+}
+
+void UCACEtherAPI::EtherGetCallBack(FHttpRequestPtr req, FHttpResponsePtr res)
+{
+    FString JsonString = res->GetContentAsString();
+    FEtherResponse ParsedResponse = JsonPerse<FEtherResponse>(JsonString);
+
+    if (ParsedResponse.status == TEXT("success"))
+    {
+        OnEtherGetCallBack(ParsedResponse.data);
+    }
+    else
+    {
+        OnFailEtherGetCallBack(TEXT("에테르 조회에 실패했습니다."));
+    }
+}
+
+void UCACEtherAPI::EtherListGetCall()
+{
+    this->API = TEXT("api/ether/list");
+    HttpGetCall();
+}
+
+void UCACEtherAPI::EtherListGetCallBack(FHttpRequestPtr req, FHttpResponsePtr res)
+{
+    FString JsonString = res->GetContentAsString();
+    FEtherListResponse ParsedResponse = JsonPerse<FEtherListResponse>(JsonString);
+
+    if (ParsedResponse.status == TEXT("success"))
+    {
+        OnEtherListGetCallBack(ParsedResponse.data);
+    }
+    else
+    {
+        OnFailEtherListGetCallBack();
+    }
+}
+
+void UCACEtherAPI::EtherHistoryGetCall(const FString& UserId)
+{
+    this->API = FString::Printf(TEXT("api/ether/history/%s"), *UserId);
+    HttpGetCall();
+}
+
+void UCACEtherAPI::EtherHistoryGetCallBack(FHttpRequestPtr req, FHttpResponsePtr res)
+{
+    FString JsonString = res->GetContentAsString();
+    FEtherBalanceResponse ParsedResponse = JsonPerse<FEtherBalanceResponse>(JsonString);
+
+    if (ParsedResponse.status == TEXT("success"))
+    {
+        OnEtherHistoryGetCallBack(ParsedResponse.data);
+    }
+    else
+    {
+        OnFailEtherHistoryGetCallBack();
+    }
 }
---
+++ VRGuild/Source/VRGuild/Public/Global/API/BPL/CBPLEther.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Global/API/BPL/CBPLBase.h"
+#include "CBPLEther.generated.h"
+
+// Single Ether Response Structure
+USTRUCT(BlueprintType)
+struct FEtherResponse : public FBaseResponse
+{
+    GENERATED_USTRUCT_BODY()
+public:
+    UPROPERTY(BlueprintReadWrite, Category = "API Response")
+    FEther data;
+};
+
+// Ether List Response Structure
+USTRUCT(BlueprintType)
+struct FEtherListResponse : public FBaseResponse
+{
+    GENERATED_USTRUCT_BODY()
+public:
+    UPROPERTY(BlueprintReadWrite, Category = "API Response")
+    TArray<FEther> data;
+};
+
+// Ether Balance Response Structure (using FEther instead of FEtherBalance)
+USTRUCT(BlueprintType)
+struct FEtherBalanceResponse : public FBaseResponse
+{
+    GENERATED_USTRUCT_BODY()
+public:
+    UPROPERTY(BlueprintReadWrite, Category = "API Response")
+    FEther data;
+};
+
+UCLASS()
+class VRGUILD_API UCBPLEther : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+};
