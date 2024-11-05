e4342884b18ac839ed32ec6821a8a4f44dc0c42a
liebespaar93 <liebespaar93@gmail.com>
2024-11-05 14:02:39 +0900

    add character api

---
+++ VRGuild/Source/VRGuild/Public/Global/API/BPL/CBPLCharacter.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Global/API/BPL/CBPLBase.h"
+#include "CBPLCharacter.generated.h"
+
+// Character Response Structure
+USTRUCT(BlueprintType)
+struct FCharacterDetailResponse : public FBaseResponse
+{
+    GENERATED_USTRUCT_BODY()
+public:
+    UPROPERTY(BlueprintReadWrite, Category = "API Response")
+    FCharacterInfo data;
+};
+
+UCLASS()
+class VRGUILD_API UCBPLCharacter : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+};
---
+++ VRGuild/Source/VRGuild/Public/Global/API/CACCharacterAPI.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Global/API/CACBaseAPI.h"
+#include "CACCharacterAPI.generated.h"
+
+struct FCharacterDetailResponse;
+struct FCharacterInfo;
+
+
+UCLASS(Blueprintable, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class VRGUILD_API UCACCharacterAPI : public UCACBaseAPI
+{
+    GENERATED_BODY()
+
+public:
+    UCACCharacterAPI();
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void InitializeComponent() override;
+    virtual void OnSuccessAPI(FHttpRequestPtr req, FHttpResponsePtr res) override;
+    virtual void OnFailAPI(FHttpRequestPtr req, FHttpResponsePtr res) override;
+
+    // Create Character
+    UFUNCTION(BlueprintCallable, Category = "Character API")
+    void CharacterCreateCall(const FCharacterInfo& CharacterInfo);
+    void CharacterCreateCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Character API")
+    void OnCharacterCreateCallBack(const FCharacterInfo& CharacterInfo);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Character API")
+    void OnFailCharacterCreateCallBack();
+
+    // Get Character
+    UFUNCTION(BlueprintCallable, Category = "Character API")
+    void CharacterGetCall(const FString& CharacterId);
+    void CharacterGetCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Character API")
+    void OnCharacterGetCallBack(const FCharacterInfo& CharacterInfo);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Character API")
+    void OnFailCharacterGetCallBack();
+
+    // Update Character
+    UFUNCTION(BlueprintCallable, Category = "Character API")
+    void CharacterUpdateCall(const FString& CharacterId, const FCharacterInfo& CharacterInfo);
+    void CharacterUpdateCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Character API")
+    void OnCharacterUpdateCallBack(const FCharacterInfo& CharacterInfo);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Character API")
+    void OnFailCharacterUpdateCallBack();
+
+    // Get Character By UserId
+    UFUNCTION(BlueprintCallable, Category = "Character API")
+    void CharacterGetByUserIdCall(const FString& UserId);
+    void CharacterGetByUserIdCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Character API")
+    void OnCharacterGetByUserIdCallBack(const FCharacterInfo& CharacterInfo);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Character API")
+    void OnFailCharacterGetByUserIdCallBack();
+
+    // Get Character By Nickname
+    UFUNCTION(BlueprintCallable, Category = "Character API")
+    void CharacterGetByNicknameCall(const FString& Nickname);
+    void CharacterGetByNicknameCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Character API")
+    void OnCharacterGetByNicknameCallBack(const FCharacterInfo& CharacterInfo);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Character API")
+    void OnFailCharacterGetByNicknameCallBack();
+};
