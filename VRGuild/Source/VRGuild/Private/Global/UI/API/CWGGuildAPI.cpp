14db6914ba06c2df993e2af367a7ea1c1183b8b8
[Spacialist] <a01050254473@gmail.com>
2024-11-06 13:10:12 +0900

    fix Guild UI API connect (2)
    길드 창설 Data Post call API 연동 / Guild 창설 팝업 UI 추가

---
+++ VRGuild/Source/VRGuild/Private/Global/UI/API/CWGGuildAPI.cpp
@@ -0,0 +1,158 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Global/UI/API/CWGGuildAPI.h"
+#include "Global/API/BPL/CBPLGuild.h"
+
+void UCWGGuildAPI::OnSuccessAPI(FHttpRequestPtr req, FHttpResponsePtr res)
+{
+    UE_LOG(LogTemp, Display, TEXT("OnSuccessAPI : %s"), *req->GetURL());
+
+    FRegexPattern CreateGuildPattern(TEXT(R"(POST\s+/api/guild$)"));
+    FRegexPattern GetGuildPattern(TEXT(R"(GET\s+/api/guild/(\d+)$)"));
+    FRegexPattern UpdateGuildPattern(TEXT(R"(PATCH\s+/api/guild/(\d+)$)"));
+    FRegexPattern ApplyGuildPattern(TEXT(R"(GET\s+/api/guild/apply/(\d+)$)"));
+
+    FString UrlToMatch = req->GetVerb() + TEXT(" /") + GetAPIPath(req->GetURL());
+    UE_LOG(LogTemp, Display, TEXT("URL to match: %s"), *UrlToMatch);
+
+    if (FRegexMatcher(CreateGuildPattern, UrlToMatch).FindNext())
+    {
+        GuildCreateCallBack(req, res);
+    }
+    else if (FRegexMatcher(GetGuildPattern, UrlToMatch).FindNext())
+    {
+        GuildGetCallBack(req, res);
+    }
+    else if (FRegexMatcher(UpdateGuildPattern, UrlToMatch).FindNext())
+    {
+        GuildUpdateCallBack(req, res);
+    }
+    else if (FRegexMatcher(ApplyGuildPattern, UrlToMatch).FindNext())
+    {
+        GuildApplyCallBack(req, res);
+    }
+}
+
+void UCWGGuildAPI::OnFailAPI(FHttpRequestPtr req, FHttpResponsePtr res)
+{
+    FString UrlToMatch = req->GetVerb() + TEXT(" /") + GetAPIPath(req->GetURL());
+
+    FRegexPattern CreateGuildPattern(TEXT(R"(POST\s+/api/guild$)"));
+    FRegexPattern GetGuildPattern(TEXT(R"(GET\s+/api/guild/(\d+)$)"));
+    FRegexPattern UpdateGuildPattern(TEXT(R"(PATCH\s+/api/guild/(\d+)$)"));
+    FRegexPattern ApplyGuildPattern(TEXT(R"(GET\s+/api/guild/apply/(\d+)$)"));
+
+    if (FRegexMatcher(CreateGuildPattern, UrlToMatch).FindNext())
+    {
+        OnFailGuildCreateCallBack();
+    }
+    else if (FRegexMatcher(GetGuildPattern, UrlToMatch).FindNext())
+    {
+        OnFailGuildGetCallBack();
+    }
+    else if (FRegexMatcher(UpdateGuildPattern, UrlToMatch).FindNext())
+    {
+        OnFailGuildUpdateCallBack();
+    }
+    else if (FRegexMatcher(ApplyGuildPattern, UrlToMatch).FindNext())
+    {
+        FString ErrorMessage = TEXT("알 수 없는 오류가 발생했습니다.");
+        if (res->GetResponseCode() == 400 || res->GetResponseCode() == 401)
+        {
+            FString JsonString = res->GetContentAsString();
+            TSharedPtr<FJsonObject> JsonObject;
+            TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(JsonString);
+            if (FJsonSerializer::Deserialize(Reader, JsonObject))
+            {
+                ErrorMessage = JsonObject->GetStringField(TEXT("message"));
+            }
+        }
+        OnFailGuildApplyCallBack(ErrorMessage);
+    }
+}
+
+void UCWGGuildAPI::GuildCreateCall(const FGuildInfo& GuildInfo)
+{
+    this->API = TEXT("api/guild");
+    HttpPostCall<FGuildInfo>(GuildInfo);
+}
+
+void UCWGGuildAPI::GuildCreateCallBack(FHttpRequestPtr req, FHttpResponsePtr res)
+{
+    FString JsonString = res->GetContentAsString();
+    FGuildDetailResponse ParsedResponse = JsonPerse<FGuildDetailResponse>(JsonString);
+
+    if (ParsedResponse.status == TEXT("success"))
+    {
+        OnGuildCreateCallBack(ParsedResponse.data);
+    }
+    else
+    {
+        OnFailGuildCreateCallBack();
+    }
+}
+
+void UCWGGuildAPI::GuildGetCall(const FString& GuildId)
+{
+    this->API = FString::Printf(TEXT("api/guild/%s"), *GuildId);
+    HttpGetCall();
+}
+
+void UCWGGuildAPI::GuildGetCallBack(FHttpRequestPtr req, FHttpResponsePtr res)
+{
+    FString JsonString = res->GetContentAsString();
+    FGuildDetailResponse ParsedResponse = JsonPerse<FGuildDetailResponse>(JsonString);
+
+    if (ParsedResponse.status == TEXT("success"))
+    {
+        OnGuildGetCallBack(ParsedResponse.data);
+    }
+    else
+    {
+        OnFailGuildGetCallBack();
+    }
+}
+
+void UCWGGuildAPI::GuildUpdateCall(const FString& GuildId, const FGuildInfo& GuildInfo)
+{
+    this->API = FString::Printf(TEXT("api/guild/%s"), *GuildId);
+    HttpPatchCall<FGuildInfo>(GuildInfo);
+}
+
+void UCWGGuildAPI::GuildUpdateCallBack(FHttpRequestPtr req, FHttpResponsePtr res)
+{
+    FString JsonString = res->GetContentAsString();
+    FGuildDetailResponse ParsedResponse = JsonPerse<FGuildDetailResponse>(JsonString);
+
+    if (ParsedResponse.status == TEXT("success"))
+    {
+        OnGuildUpdateCallBack(ParsedResponse.data);
+    }
+    else
+    {
+        OnFailGuildUpdateCallBack();
+    }
+}
+
+void UCWGGuildAPI::GuildApplyCall(const FString& GuildId)
+{
+    this->API = FString::Printf(TEXT("api/guild/apply/%s"), *GuildId);
+    HttpGetCall();
+}
+
+void UCWGGuildAPI::GuildApplyCallBack(FHttpRequestPtr req, FHttpResponsePtr res)
+{
+    FString JsonString = res->GetContentAsString();
+    FGuildDetailResponse ParsedResponse = JsonPerse<FGuildDetailResponse>(JsonString);
+
+    if (ParsedResponse.status == TEXT("success"))
+    {
+        OnGuildApplyCallBack(ParsedResponse.data);
+    }
+    else
+    {
+        OnFailGuildApplyCallBack(TEXT("API 호출에 실패했습니다."));
+    }
+}
+
---
+++ VRGuild/Source/VRGuild/Public/Global/UI/API/CWGGuildAPI.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Global/UI/API/CWGBaseAPI.h"
+#include "CWGGuildAPI.generated.h"
+
+struct FGuildDetailResponse;
+struct FGuildInfo;
+/**
+ * 
+ */
+UCLASS()
+class VRGUILD_API UCWGGuildAPI : public UCWGBaseAPI
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void OnSuccessAPI(FHttpRequestPtr req, FHttpResponsePtr res) override;
+    virtual void OnFailAPI(FHttpRequestPtr req, FHttpResponsePtr res) override;
+
+    // Create Guild
+    UFUNCTION(BlueprintCallable, Category = "Guild API")
+    void GuildCreateCall(const FGuildInfo& GuildInfo);
+    void GuildCreateCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Guild API")
+    void OnGuildCreateCallBack(const FGuildInfo& GuildInfo);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Guild API")
+    void OnFailGuildCreateCallBack();
+
+    // Get Guild
+    UFUNCTION(BlueprintCallable, Category = "Guild API")
+    void GuildGetCall(const FString& GuildId);
+    void GuildGetCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Guild API")
+    void OnGuildGetCallBack(const FGuildInfo& GuildInfo);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Guild API")
+    void OnFailGuildGetCallBack();
+
+    // Update Guild
+    UFUNCTION(BlueprintCallable, Category = "Guild API")
+    void GuildUpdateCall(const FString& GuildId, const FGuildInfo& GuildInfo);
+    void GuildUpdateCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Guild API")
+    void OnGuildUpdateCallBack(const FGuildInfo& GuildInfo);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Guild API")
+    void OnFailGuildUpdateCallBack();
+
+    // Apply To Guild
+    UFUNCTION(BlueprintCallable, Category = "Guild API")
+    void GuildApplyCall(const FString& GuildId);
+    void GuildApplyCallBack(FHttpRequestPtr req, FHttpResponsePtr res);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Guild API")
+    void OnGuildApplyCallBack(const FGuildInfo& GuildInfo);
+    UFUNCTION(BlueprintImplementableEvent, Category = "Guild API")
+    void OnFailGuildApplyCallBack(const FString& ErrorMessage);
+	
+};
