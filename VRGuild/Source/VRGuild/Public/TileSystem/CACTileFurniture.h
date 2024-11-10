2959ad4f7c4bd223f36379343b3b28c431ef87d3
[Spacialist] <a01050254473@gmail.com>
2024-11-10 11:47:57 +0900

    Feat : Scroll Tile Furniture
    가구 선택 시 동적으로 Scroll 추가할 수 있는 위젯 생성

---
+++ VRGuild/Source/VRGuild/Public/TileSystem/CACTileFurniture.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CACTileFurniture.generated.h"
+
+USTRUCT(BlueprintType, Atomic)
+struct FTileFurnitureInfo
+{
+	GENERATED_USTRUCT_BODY()
+public:
+	FTileFurnitureInfo(){};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class TSubclassOf<AActor> TileFurniture;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	class UTexture2D* CustomImage;
+};
+
+UCLASS( Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class VRGUILD_API UCACTileFurniture : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UCACTileFurniture();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TMap<int, FTileFurnitureInfo> TileFurnitureBox;
+
+	UFUNCTION(BlueprintCallable)
+	void SwitchFurniture(int value);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
