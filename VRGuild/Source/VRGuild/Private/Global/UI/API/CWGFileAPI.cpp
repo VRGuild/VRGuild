089915364b4a17d104de6d6abb7ae6821d24a7e3
liebespaar93 <liebespaar93@gmail.com>
2024-11-13 13:45:54 +0900

    merge origin dev

--- VRGuild/Source/VRGuild/Private/Global/UI/API/CWGFileAPI.cpp
+++ VRGuild/Source/VRGuild/Private/Global/UI/API/CWGFileAPI.cpp
@@ -4,6 +4,9 @@
 #include "Global/UI/API/CWGFileAPI.h"
 #include "Global/API/BPL/CBPLFile.h"
 
+#include "Misc/FileHelper.h"
+#include "HAL/PlatformFilemanager.h"
+
 TArray<uint8> FStringToUint8(const FString& InString)
 {
     TArray<uint8> OutBytes;
@@ -28,6 +31,7 @@ FString UCWGFileAPI::AddData(FString Name, FString Value)
         + Value;
 }
 
+
 void UCWGFileAPI::UploadFile(const FFileData& FullFilePath)
 {
     FHttpModule& HttpModule = FHttpModule::Get();
@@ -37,7 +41,7 @@ void UCWGFileAPI::UploadFile(const FFileData& FullFilePath)
     HttpRequest->SetURL(this->URL + "api/files/seper");
 
     HttpRequest->SetVerb(TEXT("POST"));
-
+    
     // Create a boundary label, for the header
     BoundaryLabel = FString(TEXT("---BoundaryString")) + FString::FromInt(FMath::Rand());
     // boundary label for begining of every payload chunk 
@@ -58,7 +62,7 @@ void UCWGFileAPI::UploadFile(const FFileData& FullFilePath)
         + "CustomImage" + "\"\r\n"
         + "Content-Type: image/png"
         + FString(TEXT("\r\n\r\n"));
-
+    
     // Notice, we convert all strings into uint8 format using FStringToUint8
     CombinedContent.Append(FStringToUint8(FileBoundaryString));
 
@@ -98,5 +102,177 @@ void UCWGFileAPI::UploadFile(const FFileData& FullFilePath)
         });
 
     // Send the request 
+    HttpRequest->ProcessRequest();
+}
+
+
+void UCWGFileAPI::UploadMultyFile(const FFileInfoDatas& FileInfos)
+{
+    FHttpModule& HttpModule = FHttpModule::Get();
+    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = HttpModule.CreateRequest();
+
+    HttpRequest->SetURL(this->URL + "api/files");
+    HttpRequest->SetVerb(TEXT("POST"));
+
+    BoundaryLabel = FString(TEXT("---BoundaryString")) + FString::FromInt(FMath::Rand());
+    BoundaryBegin = FString(TEXT("--")) + BoundaryLabel + FString(TEXT("\r\n"));
+    BoundaryEnd = FString(TEXT("\r\n--")) + BoundaryLabel + FString(TEXT("--\r\n"));
+
+    HttpRequest->SetHeader(TEXT("Content-Type"),
+        FString::Printf(TEXT("multipart/form-data; boundary=%s"), *BoundaryLabel));
+
+    TArray<uint8> CombinedContent;
+    TArray<uint8> OutBinaryData;
+
+    // 각 파일 정보 처리
+    for (const FFileInfo& FileInfo : FileInfos.multipartFiles)
+    {
+        if (!FPlatformFileManager::Get().GetPlatformFile().FileExists(*FileInfo.fileName))
+        {
+            UE_LOG(LogTemp, Error, TEXT("File does not exist: %s"), *FileInfo.fileName);
+            continue;
+        }
+        // 파일 데이터를 바이너리 배열로 읽어오기
+        if (!FFileHelper::LoadFileToArray(OutBinaryData, *FileInfo.fileName))
+        {
+            UE_LOG(LogTemp, Error, TEXT("Failed to load file: %s"), *FileInfo.fileName);
+            continue ;
+        }
+
+        // 경로 정리
+        FString CleanFileName = FPaths::GetCleanFilename(FileInfo.fileName);  // 파일 이름만 추출
+        FString NormalizedPath = FPaths::ConvertRelativePathToFull(FileInfo.filePath);  // 전체 경로로 변환
+        FPaths::NormalizeFilename(NormalizedPath);  // 경로 정규화
+
+        // 파일 확장자와 Content-Type 결정
+        FString Extension = FPaths::GetExtension(FileInfo.fileName).ToLower();
+        FString ContentType = TEXT("application/octet-stream");  // 기본값
+
+        // 파일 타입에 따른 Content-Type 설정
+        if (Extension == TEXT("jpg") || Extension == TEXT("jpeg"))
+            ContentType = TEXT("image/jpeg");
+        else if (Extension == TEXT("png"))
+            ContentType = TEXT("image/png");
+        else if (Extension == TEXT("pdf"))
+            ContentType = TEXT("application/pdf");
+        else if (Extension == TEXT("txt"))
+            ContentType = TEXT("text/plain");
+        // 필요한 다른 파일 타입들 추가...
+
+        // 파일 경계 문자열 생성
+        FString FileBoundary = TEXT("\r\n") + BoundaryBegin +
+            TEXT("Content-Disposition: form-data; name=\"multipartFiles\"; filename=\"") +
+            CleanFileName + TEXT("\"\r\n") +
+            TEXT("Content-Type: ") + ContentType + TEXT("\r\n\r\n");
+
+        FTCHARToUTF8 BoundaryConverter(*FileBoundary);
+        CombinedContent.Append((uint8*)BoundaryConverter.Get(), BoundaryConverter.Length());
+        CombinedContent.Append(OutBinaryData);
+    }
+
+    CombinedContent.Append(FStringToUint8(AddData("filePath", FileInfos.filePath + "/")));
+
+    // 마지막 경계 추가
+    CombinedContent.Append(FStringToUint8(BoundaryEnd));
+
+    HttpRequest->SetContent(CombinedContent);
+
+    HttpRequest->OnProcessRequestComplete().BindLambda(
+        [this](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess)
+        {
+            if (bSuccess && Response.IsValid())
+            {
+                FString Result = Response->GetContentAsString();
+                OnFileUploadCallBack(Result);
+            }
+            else
+            {
+                OnFailFileUploadCallBack();
+                if (!bSuccess)
+                {
+                    UE_LOG(LogTemp, Error, TEXT("Upload failed: Connection error"));
+                }
+                else
+                {
+                    UE_LOG(LogTemp, Error, TEXT("Upload failed: Invalid response"));
+                }
+            }
+        });
+
+    HttpRequest->ProcessRequest();
+}
+
+void UCWGFileAPI::UploadMultyBinary(const FFileDatas& FullFilePath)
+{
+    FHttpModule& HttpModule = FHttpModule::Get();
+    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = HttpModule.CreateRequest();
+
+    HttpRequest->SetURL(this->URL + "api/files");
+    HttpRequest->SetVerb(TEXT("POST"));
+
+    BoundaryLabel = FString(TEXT("---BoundaryString")) + FString::FromInt(FMath::Rand());
+    BoundaryBegin = FString(TEXT("--")) + BoundaryLabel + FString(TEXT("\r\n"));
+    BoundaryEnd = FString(TEXT("\r\n--")) + BoundaryLabel + FString(TEXT("--\r\n"));
+
+    HttpRequest->SetHeader(TEXT("Content-Type"),
+        FString::Printf(TEXT("multipart/form-data; boundary=%s"), *BoundaryLabel));
+
+    TArray<uint8> CombinedContent;
+
+    for (const FFileData& File : FullFilePath.multipartFiles)
+    {
+        // 파일 확장자와 Content-Type 결정
+        FString Extension = FPaths::GetExtension(File.fileName).ToLower();
+        FString ContentType = TEXT("application/octet-stream");  // 기본값
+
+        // 파일 타입에 따른 Content-Type 설정
+        if (Extension == TEXT("jpg") || Extension == TEXT("jpeg"))
+            ContentType = TEXT("image/jpeg");
+        else if (Extension == TEXT("png"))
+            ContentType = TEXT("image/png");
+        else if (Extension == TEXT("pdf"))
+            ContentType = TEXT("application/pdf");
+        else if (Extension == TEXT("txt"))
+            ContentType = TEXT("text/plain");
+        // 필요한 다른 파일 타입들 추가...
+
+        // 파일 경계 문자열 생성 (Content-Type 포함)
+        FString FileBoundary = TEXT("\r\n") + BoundaryBegin +
+            TEXT("Content-Disposition: form-data; name=\"multipartFiles\"; filename=\"") +
+            File.fileName + TEXT("\"\r\n") +
+            TEXT("Content-Type: ") + ContentType + TEXT("\r\n\r\n");
+
+        FTCHARToUTF8 BoundaryConverter(*FileBoundary);
+        CombinedContent.Append((uint8*)BoundaryConverter.Get(), BoundaryConverter.Length());
+        CombinedContent.Append(File.multipartFiles);
+    }
+
+    CombinedContent.Append(FStringToUint8(AddData("filePath", FullFilePath.filePath + "/")));
+    CombinedContent.Append(FStringToUint8(BoundaryEnd));
+
+    HttpRequest->SetContent(CombinedContent);
+
+    HttpRequest->OnProcessRequestComplete().BindLambda(
+        [this](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess)
+        {
+            if (bSuccess && Response.IsValid())
+            {
+                FString Result = Response->GetContentAsString();
+                OnFileUploadCallBack(Result);
+            }
+            else
+            {
+                OnFailFileUploadCallBack();
+                if (!bSuccess)
+                {
+                    UE_LOG(LogTemp, Error, TEXT("Upload failed: Connection error"));
+                }
+                else
+                {
+                    UE_LOG(LogTemp, Error, TEXT("Upload failed: Invalid response"));
+                }
+            }
+        });
+
     HttpRequest->ProcessRequest();
 }
--- VRGuild/Source/VRGuild/Public/Global/API/BPL/CBPLFile.h
+++ VRGuild/Source/VRGuild/Public/Global/API/BPL/CBPLFile.h
@@ -9,17 +9,54 @@
 
 
 
+// FFileData 구조체도 업데이트 필요
 USTRUCT(BlueprintType)
 struct FFileData
 {
     GENERATED_USTRUCT_BODY()
-public:
-    UPROPERTY(BlueprintReadWrite, Category = "Comment|User")
+
+    UPROPERTY(BlueprintReadWrite, Category = "File")
     TArray<uint8> multipartFiles;
-    UPROPERTY(BlueprintReadWrite, Category = "Comment|User")
-    FString PathFile;
+
+    UPROPERTY(BlueprintReadWrite, Category = "File")
+    FString fileName;  // 파일 이름 필드 추가
 };
 
+
+
+USTRUCT(BlueprintType)
+struct FFileDatas
+{
+    GENERATED_USTRUCT_BODY()
+public:
+    UPROPERTY(BlueprintReadWrite, Category = "Comment|User")
+    TArray<FFileData> multipartFiles;
+    UPROPERTY(BlueprintReadWrite, Category = "Comment|User")
+    FString filePath;
+};
+
+USTRUCT(BlueprintType)
+struct FFileInfo
+{
+    GENERATED_USTRUCT_BODY()
+
+    UPROPERTY(BlueprintReadWrite, Category = "File")
+    FString filePath;
+
+    UPROPERTY(BlueprintReadWrite, Category = "File")
+    FString fileName;  // 파일 이름 필드 추가
+};
+
+USTRUCT(BlueprintType)
+struct FFileInfoDatas
+{
+    GENERATED_USTRUCT_BODY()
+public:
+    UPROPERTY(BlueprintReadWrite, Category = "Comment|User")
+    TArray<FFileInfo> multipartFiles;
+    UPROPERTY(BlueprintReadWrite, Category = "Comment|User")
+    FString filePath;
+};
 
 /**
  * 
