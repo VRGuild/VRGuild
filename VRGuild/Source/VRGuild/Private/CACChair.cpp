1417069468de4f9c2c96a5d768fd443c21b166ee
[Spacialist] <a01050254473@gmail.com>
2024-11-18 20:39:45 +0900

    Feat : NPC Player
    npc player 9명 추가 (Level 변경 후 배치 예정)
    움직이는 AI npc는 시간 여유되면 Patrol 수정 예정

---
+++ VRGuild/Source/VRGuild/Private/CACChair.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CACChair.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ACACChair::ACACChair()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	SetRootComponent(BoxComp);
+
+	ChairArrow = CreateDefaultSubobject<USceneComponent>(TEXT("ChairArrow"));
+	ChairArrow->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ACACChair::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACACChair::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ VRGuild/Source/VRGuild/Public/CACChair.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CACChair.generated.h"
+
+UCLASS()
+class VRGUILD_API ACACChair : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACACChair();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+	class USceneComponent* ChairArrow;
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite)
+	class UBoxComponent* BoxComp;
+};
